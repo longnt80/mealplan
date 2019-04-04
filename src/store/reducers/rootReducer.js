5ed9eaa3416619c60679c5e7a6f82fed5507f6cd
Long Nguyen <longnt80@gmail.com>
2019-04-05 09:13:47 +1100

    deleted unneccessary contants

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import appReducer from '../../components/app/reducer/appReducer';
 import recipesReducer from '../../components/recipes/reducer';
-import authReducer from '../../components/app/reducer/authReducer';
 
 const rootReducer = combineReducers({
   appStatus: appReducer,
-  auth: authReducer,
   recipes: recipesReducer,
 });
 
